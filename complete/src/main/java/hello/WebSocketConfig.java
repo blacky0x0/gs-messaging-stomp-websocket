fb6745acf218ad1a436fa3a8b51d8ad4c6c62ef4
Alexey Dushen <dushen.alexey@gmail.com>
2017-07-23 23:34:54 +0300

    ws & EchoHandler

--- complete/src/main/java/hello/WebSocketConfig.java
+++ complete/src/main/java/hello/WebSocketConfig.java
@@ -1,5 +1,37 @@
-//package hello;
-//
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new EchoHandler(), "/ws").setAllowedOrigins("*").withSockJS();
+    }
+
+
+    public static class EchoHandler extends TextWebSocketHandler {
+        @Override
+        public void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws IOException {
+            session.sendMessage(new TextMessage("pong: received: " + textMessage.getPayload()));
+        }
+    }
+
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(new SocketHandler(), "/name");
+//    }
+}
+
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 //import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -18,7 +50,7 @@
 //
 //    @Override
 //    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+//        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
 //    }
 //
 //}
